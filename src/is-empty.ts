8908849e40b00f7a1b9e45348e4b73e9f36b17ba
Tristan Strathearn <tristan.strathearn@growthops.com.au>
2021-08-16 11:51:51 +1000

    feat: convert isEmpty function to guard clause

--- src/is-empty.ts
+++ src/is-empty.ts
@@ -1,15 +1,22 @@
+import {isArray, isString, isObject} from 'remeda';
+
 type Emptiable = string | Record<any, any> | any[];
 
-const isEmpty = (input: Emptiable): boolean => {
-	if (typeof input === 'string') {
+// eslint-disable-next-line @typescript-eslint/ban-types
+const isEmpty = <T>(input: T): input is Extract<T, '' | [] | {}> => {
+	if (isString(input)) {
 		return input === '';
 	}
 
-	if (Array.isArray(input)) {
+	if (isArray(input)) {
 		return input.length === 0;
 	}
 
-	return Object.keys(input).length === 0;
+	if (isObject(input)) {
+		return Object.keys(input).length === 0;
+	}
+
+	return false;
 };
 
 export default isEmpty;
