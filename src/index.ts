28afc3d32f62fd473441bee76971cfb1b327e8c5
Tristan Strathearn <tristan.strathearn@growthops.com.au>
2021-10-20 15:33:58 +1000

    fix: add missing isPopulated and notPopulated function exports to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-export {default as replace} from './replace';
-export {default as trim} from './trim';
-export {default as join} from './join';
 export {default as collapse} from './collapse';
 export {default as isEmpty} from './is-empty';
+export {default as isPopulated} from './is-populated';
+export {default as join} from './join';
 export {default as notEmpty} from './not-empty';
 export {default as notEquals} from './not-equals';
 export {default as notNil} from './not-nil';
+export {default as notPopulated} from './not-populated';
+export {default as replace} from './replace';
+export {default as trim} from './trim';
 export {default as useOnScreen} from './use-on-screen';
 
 export type {Collapsible} from './collapse';
